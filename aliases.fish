23b8a13608a101eaaf823e4ca488343c29044baa
Chris Butler <chris@platformthirteen.com>
2018-01-14 17:50:06 -0500

    add gitconfig and fish aliases

---
+++ aliases.fish
@@ -0,0 +1,14 @@
+alias gst='git st'
+
+alias gcp='git commit -p'
+
+alias gps='git push'
+alias gpl='git pull'
+
+alias bi='bundle install'
+alias be='bundle exec'
+
+alias gbs='git br -v'
+
+alias gpu='git pull upstream'
+alias gpum='git pull upstream master'
